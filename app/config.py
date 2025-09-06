21f9d5c37ca4ec8f9e1484c9d1fd9b93c6be5185
Armin Mozaffari <mylyzerteam@gmail.com>
2025-09-06 19:58:38 +0330

    config up

--- app/config.py
+++ app/config.py
@@ -93,7 +93,7 @@ class Settings:
         "(KHTML, like Gecko) Chrome/125 Safari/537.36",
     )
     rss_max_items_per_feed: int = _get_int("RSS_MAX_ITEMS_PER_FEED", 10)
-    rss_batch_size: int = _get_int("RSS_BATCH_SIZE", 5)             # چند فید در هر poll
+    rss_batch_size: int = _get_int("RSS_BATCH_SIZE", 3)             # چند فید در هر poll
     rss_fetch_concurrency: int = _get_int("RSS_FETCH_CONCURRENCY", 3) # concurrency برای fetch
 
     # --- UA جنریک ---
